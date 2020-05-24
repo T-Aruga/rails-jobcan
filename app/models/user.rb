ef0517142fe328686aecc251e0c1f87d6fd35811
T-Aruga <baideneo@gmail.com>
2020-05-24 23:05:54 +0900

    Add validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  before_validation :downcase_email
+
+  # validates
+  validates :email, presence: true, email: { allow_blank: true }
+
+  # methods
+  # 自分以外のactiveなユーザーがいればtrueを返す
+  def activated?(email)
+    users = new_record? ? User.all : User.where.not(id: self.id)
+    users.find_by(email: email, activated: true).present?
+  end
+
+  private
+    # validatesにemail小文字化
+    def downcase_email
+      self.email = email.downcase if self.email
+    end
 end
