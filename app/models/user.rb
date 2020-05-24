84b0269afbaa98ea67347f23fdf54057d063b5aa
T-Aruga <baideneo@gmail.com>
2020-05-24 23:22:00 +0900

    Add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
   before_validation :downcase_email
 
+  # password hashing
+  has_secure_password
+
   # validates
   validates :email, presence: true, email: { allow_blank: true }
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: :invalid_password
+                       },
+                       allow_blank: true
 
   # methods
   # 自分以外のactiveなユーザーがいればtrueを返す
